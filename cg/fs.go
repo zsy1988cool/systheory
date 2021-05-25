29a0d05cb43e660fe33ecb741e476c89e6648988
zsy <zsy1988cool@163.com>
2021-05-25 15:38:58 +0800

    init2

--- cg/fs.go
+++ cg/fs.go
@@ -1,6 +1,9 @@
 package cg
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type file_system_type struct {
 	f_type string
@@ -47,13 +50,18 @@ func rebind_subsystems(root* cgroupfs_root, data *string)  {
 	top_cgroup := cgroup {
 		subsys: subsys,
 		dentry: *data,
+		root: root,
 	}
 
 	root.top_cgroup = &top_cgroup
 }
 
-func cgroup_populate_dir(cgroup *cgroup)  {
-	fmt.Println("mouted dentry ", cgroup.dentry)
+
+
+var cgroupMap = make(map[string]*cgroup)
+func cgroup_populate_dir(cgrp *cgroup)  {
+	fmt.Println("mouted dentry ", cgrp.dentry)
+	cgroupMap[cgrp.dentry] = cgrp
 }
 
 func find_task_by_vpid(pid int) *task_struct  {
@@ -99,4 +107,42 @@ func attach_task_by_pid(cgrp *cgroup, pid int)  {
 
 func Mounted(fs_type string, subsys string, dir string)  {
 	cgroup_get_sb(&file_system_type{f_type: fs_type}, &dir, nil)
+}
+
+func findCgroupByDir(dir string) *cgroup {
+	return cgroupMap[dir]
+}
+func AttchTask(pid int, dir string)  {
+	index := strings.LastIndex(dir, "/")
+	realDir := Substr(dir, 0, index)
+	cgrp := findCgroupByDir(realDir)
+	attach_task_by_pid(cgrp, pid)
+}
+func Substr(str string, start, length int) string {
+	rs := []rune(str)
+	rl := len(rs)
+	end := 0
+
+	if start < 0 {
+		start = rl - 1 + start
+	}
+	end = start + length
+
+	if start > end {
+		start, end = end, start
+	}
+
+	if start < 0 {
+		start = 0
+	}
+	if start > rl {
+		start = rl
+	}
+	if end < 0 {
+		end = 0
+	}
+	if end > rl {
+		end = rl
+	}
+	return string(rs[start:end])
 }
--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("演示cgroup")
 
 	//mount -t cgroup -o memory memory /sys/fs/cgroup/memory
 	cg.Mounted("cgroup", "memory", "/sys/fs/cgroup/memory")
+
+	// echo 123012 > /sys/fs/cgroup/memory/tasks
+	cg.AttchTask(123, "/sys/fs/cgroup/memory/tasks")
 }
